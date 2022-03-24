efa89484f2b5c33d156306f08502b7100ba4a51a
SamanjaCartagena <cart.samanja09@gmail.com>
2022-03-24 14:31:08 -0400

    events added

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const app = Vue.createApp({
  data(){
    return {
-     counter:0
+     counter:0,
+     name:''
    }
  }, 
  methods:{
    add(num){
      this.counter= this.counter+num;
-   }
+   },
+   setName(event){
+     this.name = event.target.value;
+   },
  }
 });
 
